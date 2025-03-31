51a4f9d1b6f53ac1edb1d83f993f3b54db09feb9
TaohuThot <third.nk20@gmail.com>
2025-03-31 22:06:59 +0700

    .

---
+++ src/app/component/Body.tsx
@@ -0,0 +1,9 @@
+'use client';
+import React from 'react'
+
+const Body = () => {
+  return (
+    <section>Home</section>
+  )
+}
+export default Body;
---
+++ src/app/component/Carousel.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const images = [
+  "https://images.unsplash.com/photo-1741851373559-6879db14fd8a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1742077414023-45a81fd63736?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1742137188175-d31977b9561d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative w-full mx-auto">
+      <div className="overflow-hidden ">
+        <img
+          src={images[currentIndex]}
+          alt="Carousel"
+          className="w-full h-200 object-cover transition-opacity duration-500"
+        />
+      </div>
+
+      {/* Navigation Dots */}
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {images.map((_, index) => ( 
+          <button
+            key={index}
+            className={`w-3 h-3 rounded-full ${
+              currentIndex === index ? "bg-white" : "bg-gray-400"
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
